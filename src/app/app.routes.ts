7dd9bcb95023c52ff1c2c25e69fac8e73409d65f
Stepan Yefimov <stepan.yefimov@gmail.com>
2025-04-26 17:59:21 +0000

    Initial changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { MoonDaysComponentComponent } from './moon-days-component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    title: 'Moon Days',
+    path: '',
+    component: MoonDaysComponentComponent,
+  },
+];
---
+++ src/app/moon-days-component/moon-days-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-moon-days-component',
+  imports: [],
+  templateUrl: './moon-days-component.component.html',
+  styleUrl: './moon-days-component.component.scss'
+})
+export class MoonDaysComponentComponent {
+
+}
